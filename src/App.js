ba2fdfa9b61cc3e10652cd6530acfafaa01ccc84
Harsh <harshsharma102000@gmail.com>
2021-04-07 16:34:22 +0530

    Few Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import Details from "./components/Details/Details";
+import Main from "./components/Main/Main";
 import useStyles from "./styles";
 
 const App = () => {
@@ -8,7 +9,7 @@ const App = () => {
   return (
     <div>
       <Grid
-        className={classes.Grid}
+        className={classes.grid}
         container
         spacing={0}
         alignItems="center"
@@ -18,11 +19,11 @@ const App = () => {
         <Grid item xs={12} sm={4}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={4}>
-          Main
+        <Grid item xs={12} sm={3}>
+          <Main />
         </Grid>
         <Grid item xs={12} sm={4}>
-          <Details title="Expense"/>
+          <Details title="Expense" />
         </Grid>
       </Grid>
     </div>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,24 +1,38 @@
-import { makeStyles } from "@material-ui/core";
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
 
-export default makeStyles((theme) => ({
-  media: {
-    height: 0,
-    paddingTop: "56.25%", // 16:9
-  },
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: "rotate(180deg)",
-  },
-  cartContent: {
-    paddingTop: 0,
-  },
-  divider: {
-    margin: "20px 0",
-  },
-}));
+import useStyles from "./styles";
+
+const Main = () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+      <CardContent>
+        <Typography align="center" variant="h5">Total Balance $100</Typography>
+        <Typography variant="subtitle1" style={{lineHeight:'1.5em',marginTop:'20px'}}>Total Balance $100
+        {/* {INFOR CARD} */}
+        Try saying: Add income for $100 in Category Salary for Monday...
+        </Typography>
+      <Divider/>
+      {/* {FORM} */}
+      </CardContent>
+      <CardContent className={classes.cardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            {/* <List/> */}
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Main;
