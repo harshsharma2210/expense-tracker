7a5d7f62736ea783847bb26d491947e5de8ab813
Harsh <harshsharma102000@gmail.com>
2021-04-11 09:01:53 +0530

    Files Changed

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,6 +9,8 @@ import {
 } from "@material-ui/core";
 
 import useStyles from "./styles";
+import List from "./List/List";
+import Form from "./Form/Form";
 
 const Main = () => {
   const classes = useStyles();
@@ -16,18 +18,24 @@ const Main = () => {
     <Card className={classes.root}>
       <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
       <CardContent>
-        <Typography align="center" variant="h5">Total Balance $100</Typography>
-        <Typography variant="subtitle1" style={{lineHeight:'1.5em',marginTop:'20px'}}>Total Balance $100
-        {/* {INFOR CARD} */}
-        Try saying: Add income for $100 in Category Salary for Monday...
+        <Typography align="center" variant="h5">
+          Total Balance $100
         </Typography>
-      <Divider/>
-      {/* {FORM} */}
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
+        >
+          Total Balance $100
+          {/* {INFOR CARD} */}
+          Try saying: Add income for $100 in Category Salary for Monday...
+        </Typography>
+        <Divider />
+        <Form />
       </CardContent>
       <CardContent className={classes.cardContent}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            {/* <List/> */}
+            <List />
           </Grid>
         </Grid>
       </CardContent>
