0183a2c4566b102afb8682cc80b3df7c9cdfe6e0
Harsh <harshsharma102000@gmail.com>
2021-04-12 20:48:40 +0530

    Final Updates

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,13 +1,18 @@
-const transactions = [{ id: 1 }];
-
 const contextReducer = (state, action) => {
   let transactions;
+
   switch (action.type) {
-    case "DELETE_TRANSACTION":
-      transactions = state.filter((t) => t.id !== action.payload);
+    case 'DELETE_TRANSACTION':
+      transactions = state.filter((transaction) => transaction.id !== action.payload);
+
+      localStorage.setItem('transactions', JSON.stringify(transactions));
+
       return transactions;
-    case "ADD_TRANSACTION":
-      transactions = [...state, action.payload];
+    case 'ADD_TRANSACTION':
+      transactions = [action.payload, ...state];
+
+      localStorage.setItem('transactions', JSON.stringify(transactions));
+
       return transactions;
     default:
       return state;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import App from "./App";
+import { SpeechProvider } from "@speechly/react-client";
 
 import { Provider } from "./context/context";
-
+import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
+  <SpeechProvider
+    appId=" 0cd79065-3b05-4f26-91d9-54247d4a97e3 "
+    language="en-US"
+  >
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
   document.getElementById("root")
 );
