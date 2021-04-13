804413b7b5824e2659677e640a0bd496bc0d045d
Harsh <harshsharma102000@gmail.com>
2021-04-13 09:24:32 +0530

    speechly

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
-  <SpeechProvider
-    appId=" 0cd79065-3b05-4f26-91d9-54247d4a97e3 "
-    language="en-US"
-  >
+  <SpeechProvider appId="0cd79065-3b05-4f26-91d9-54247d4a97e3" language="en-US">
     <Provider>
       <App />
     </Provider>
